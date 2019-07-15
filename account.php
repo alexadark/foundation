b335ea22b829ab579b7a8b82e9690c42e3d33faa
Vinay Paudel <paudelvinay@gmail.com>
2019-07-15 18:58:22 +0545

    managing account page

--- account.php
+++ account.php
@@ -3,5 +3,32 @@
 //Template name: Account setup
 
 $context   = Timber::get_context();
+$content_blocks_details = get_field('content_blocks');
+foreach ($content_blocks_details as $content_blocks_detail) {
+	if( $content_blocks_detail["acf_fc_layout"]=="block_booking_form" ){
+		$booking_form = $content_blocks_detail["form"];
+		break;
+	}
+}
+
+$context['form_id'] = $booking_form;
+$context['top_section'] = true;
+if( isset($_REQUEST["plan"]) && $_REQUEST["plan"]!="" ){
+	$plan = strtolower($_REQUEST["plan"]);
+	if( is_page('account-setup') ){
+		if( strpos($plan, "plan2")!==FALSE )
+			$context['form_id'] =16;
+		else 
+			$context['form_id'] =$booking_form;
+	} elseif( is_page('upgrade-subscription') ) {
+		if( strpos($plan, "plan2")!==FALSE )
+			$context['form_id'] =14;
+		else 
+			$context['form_id'] =$booking_form;
+	} else {
+		$context['form_id'] =$booking_form;
+	}
+		$context['top_section'] = false;	
+}
 $templates = array('account.twig');
 Timber::render($templates, $context);
