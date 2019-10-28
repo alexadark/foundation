13815ef7eb2785084f95d70ede0a4474e488d31f
Eric Antonio <dev.antonio.eric@gmail.com>
2019-10-28 18:59:04 +0300

    no message

--- assets/js/vendors/selectric/selectric.init.js
+++ assets/js/vendors/selectric/selectric.init.js
@@ -1,7 +1,6 @@
 (function($) {
 	$('.lab__dd select').selectric();
 	$('.search-cats select').selectric();
-	console.log('okokokok');
 	$('.selectric .button')
 		.addClass('selectricBtn')
 		.removeClass('button');
