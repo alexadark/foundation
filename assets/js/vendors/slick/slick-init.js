b76c6e4406bd6d6554a21967733a4bb9a45a915e
Eric Antonio <dev.antonio.eric@gmail.com>
2019-10-28 12:17:02 +0300

    Featured services slick slide

--- assets/js/vendors/slick/slick-init.js
+++ assets/js/vendors/slick/slick-init.js
@@ -32,4 +32,38 @@ jQuery(document).ready(function($) {
 			},
 		],
 	});
+
+	$('.featured-services__container').slick({
+		autoplay: false,
+		speed: 1000,
+		arrow: true,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
 });
